ada5bcdb08026b296d60f152083c9e036001e8d0
Yongchao Wang <ycw.puzzle@hotmail.com>
2019-01-05 22:42:29 +0800

    remove init.el and my-lisp/global-config.el from the index

--- my-lisp/global-config.el
+++
@@ -1,7 +0,0 @@
-(require 'company)
-(make-variable-buffer-local 'company-backends)
-
-(when (eq system-type 'darwin)
-  (setq mac-right-option-modifier 'control)
-  (global-set-key [kp-delete] 'delete-char)
-  )
